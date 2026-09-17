f2930448668ca12f1334d6941b0b8989f3756281
Matheus Ferraz <matheusaugustosabinoferraz.com>
2026-09-17 14:09:57 -0300

    feat: all project migration

---
+++ src/data/site.ts
@@ -0,0 +1,8 @@
+export const site = {
+  brand: 'Pioneira Rolamentos',
+  brandShort: 'Pioneira',
+  url: '/',
+  title: 'Rolamentos Industriais para Manutenção e Reposição | Pioneira Rolamentos',
+  description:
+    'Solicite cotação de rolamentos de esferas, rolos e aplicações especiais SKF, FAG, NSK e Timken. Apoio técnico na identificação e especificação para manutenção industrial.',
+}
